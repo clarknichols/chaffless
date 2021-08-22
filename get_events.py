8a63bf92ee3ea4e053879ffbfba7bdc9b40161c4
Dara Dadachanji <dara.dadachanji@gmail.com>
2021-08-22 11:23:01 -0700

    parsed initial event list
    pulling more than the 6 most recent events requires interacting with js elements.
    will probably just pull the initial list of events by hand and then refresh each day
    setting up selenium is a PITA

---
+++ get_events.py
@@ -0,0 +1,37 @@
+import requests
+import lxml.html
+import json
+
+
+def main():
+    events = fetch_events()
+    with open('data/events.json', 'w') as file:
+        json.dump(obj=events, fp=file, indent=4)
+
+
+
+def fetch_events ():
+    with open('data/links.html', mode='r', encoding='UTF-8') as file:
+        text = file.read()
+
+    page: lxml.html.HtmlElement = lxml.html.fromstring(text)
+    events = page.xpath(
+        '//div[@id="mtgo-decklists"]'
+        '/div[@class="articles-listing"]'
+        '/div'
+        '/a'
+        '/@href'
+    )
+    return events
+
+def fetch_decklists(event_url):
+    base ='https://magic.wizards.com/en/articles/archive/mtgo-standings/'
+    response = requests.get(event_url)
+    article: lxml.html.HtmlElement = lxml.html.fromstring(response.text)
+    decks = article.xpath(
+        '//div[@class="deck-list-text"]'
+    )
+
+if __name__ == '__main__':
+    main()
+    
